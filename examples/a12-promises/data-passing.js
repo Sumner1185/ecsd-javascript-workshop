e957633414793e918a5e89534a08b1a72de06788
Alastair Sumner <alastair-sumner@ecs-digital.co.uk>
2020-10-14 09:12:33 +0100

    Adding examples folder

---
+++ examples/a12-promises/data-passing.js
@@ -0,0 +1,13 @@
+const downloadData = (url) => Promise.resolve(url);
+
+downloadData("download-url")
+	.then(function(data) {
+		console.log("1st then: " + data);
+		return "formatted-data";
+	})
+	.then(function(data) {
+		console.log("2nd then: " + data);
+	})
+	.then(function(data) {
+		console.log("3rd then: " + data);
+	});
