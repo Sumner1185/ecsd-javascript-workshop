c2afb479f1781e8ef53045fd47b7da379b0fb4c2
Alastair Sumner <alastair-sumner@ecs-digital.co.uk>
2020-10-19 11:30:26 +0100

    Add pt7 exercises

--- exercises/pt6_babel_classes_inheritance/exercise-2.js
+++ exercises/pt6_babel_classes_inheritance/exercise-2.js
@@ -3,38 +3,59 @@
 // a) setters and getters.
 
 class Animal {
-  // Code here...
+  #name;
+  #age;
+  constructor(name, age) {
+    this.#name = name,
+    this.#age = age
+  }
+
+  get name() {
+    return this.#name
+  }
+
+  get age() {
+    return this.#age
+  }
+
+  set name(name) {
+    this.#name = name
+  }
 }
 
 // Uncomment to test
-// const animal = new Animal("Rex", 8);
-// console.log("Name: " + animal.name);
-// console.log("Age: " + animal.age);
-// animal.name = "Oura";
-// console.log("New Name: " + animal.name);
+const animal = new Animal("Rex", 8);
+console.log("Name: " + animal.name);
+console.log("Age: " + animal.age);
+animal.name = "Oura";
+console.log("New Name: " + animal.name);
 
 // b) Inheritance
 
-class Dog {
-
+class Dog extends Animal {
+  woof = () => {
+    return "Woof, woof"
+  }
 }
 
-class Cat {
-
+class Cat extends Animal {
+  meow = () => {
+    return "Meow, meow"
+  }
 }
 
 // Uncomment to test
 
-// const dog = new Dog("Axe", 6);
-// console.log("Dog Name: " + dog.name);
-// console.log("Dog Age: " + dog.age);
-// dog.name = "New Axe";
-// console.log("New Dog Name: " + dog.name);
-// console.log(dog.woof());
+const dog = new Dog("Axe", 6);
+console.log("Dog Name: " + dog.name);
+console.log("Dog Age: " + dog.age);
+dog.name = "New Axe";
+console.log("New Dog Name: " + dog.name);
+console.log(dog.woof());
 
-// const dog = new Cat("Napster", 3);
-// console.log("Cat Name: " + cat.name);
-// console.log("Cat Age: " + cat.age);
-// cat.name = "New Nap";
-// console.log("New Cat Name: " + cat.name);
-// console.log(cat.meow());
+const cat = new Cat("Napster", 3);
+console.log("Cat Name: " + cat.name);
+console.log("Cat Age: " + cat.age);
+cat.name = "New Nap";
+console.log("New Cat Name: " + cat.name);
+console.log(cat.meow());
